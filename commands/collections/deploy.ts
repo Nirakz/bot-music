430f648df1ef93ccb1f1b21684512abef922a2e7
Thành Chính <ntchinh271099@gmail.com>
2023-05-21 18:38:40 +0700

    Slash expansion command

---
+++ commands/collections/deploy.ts
@@ -0,0 +1,18 @@
+import { Client } from "discord.js";
+import { schema } from "../schema";
+
+export const deploy = (client: Client): void => {
+	client.on("messageCreate", async (message) => {
+		if (!message.guild) return;
+		// Allow deployment only as server owner
+		if (!client.application?.owner) await client.application?.fetch();
+		if (message.content.toLowerCase() === "!start" && message.author.id === client.application?.owner?.id) {
+			try {
+				await message.guild.commands.set(schema);
+				await message.reply("Deployed!");
+			} catch (e) {
+				message.reply("Fail to deploy!");
+			}
+		}
+	});
+};
