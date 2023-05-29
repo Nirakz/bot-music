bcdd3ae1ea94353bfdb06b677c791b6ea748de47
Thành Chính <ntchinh271099@gmail.com>
2023-05-29 18:50:00 +0700

    Feature/lint (#12)
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better
    
    * update discord.js v14
    
    * update discord.js v14
    
    * lint:fix

--- src/BotMusic.ts
+++ src/BotMusic.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 import { Client, GatewayIntentBits } from "discord.js";
 import { SoundCloud } from "scdl-core";
 import express, { Request, Response } from "express";
-import herokuAwake from 'heroku-awake';
+import herokuAwake from "heroku-awake";
 config();
 if (process.env.NODE_ENV === "production") {
 	require("module-alias/register");
@@ -13,10 +13,6 @@ export class BotMusic {
 	static init() {
 		const client = new Client({
 			intents: [
-				// Intents.FLAGS.GUILDS,
-				// Intents.FLAGS.GUILD_MESSAGES,
-				// Intents.FLAGS.GUILD_VOICE_STATES,
-				// Intents.FLAGS.GUILD_INTEGRATIONS,
 				GatewayIntentBits.Guilds,
 				GatewayIntentBits.GuildMessages,
 				GatewayIntentBits.GuildVoiceStates,
@@ -43,7 +39,7 @@ export class BotMusic {
 
 		app.listen(process.env.PORT || 3000, () => {
 			console.log(`> Bot is on listening`);
-			herokuAwake(process.env.APP_URL || 'http://localhost:3000');
+			herokuAwake(process.env.APP_URL || "http://localhost:3000");
 		});
 	}
 }
